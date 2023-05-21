73bdc3bf9602f40161501c6c1dee84a5285a682d
Andrey Kordik <andreykordik@yandex.ru>
2023-05-21 15:58:35 +0400

    12 sprint

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import Header from '../Header';
+import Header from '../Header/Header.js';
 import Footer from '../Footer/Footer.js';
 import Main from '../Main/Main.js';
 import ImagePopup from '../ImagePopup/ImagePopup.js';
@@ -10,6 +10,13 @@ import EditProfilePopup from '../../Components/EditProfilePopup/EditProfilePopup
 import EditAvatarPopup from '../EditAvatarPopup/EditAvatarPopup.js';
 import AddPlacePopup from '../AddPlacePopup/AddPlacePopup.js'
 import PopupWithConfirm from '../PopupWithConfirm/PopupWithConfirm.js'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Login from '../Login/Login.js';
+import Register from '../Register/Register.js';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute.js';
+import auth from '../../utils/MestoAuth.js'
+import { useNavigate } from 'react-router-dom';
+import InfoTooltip from '../InfoTooltip/InfoTooltip.js';
 
 function App() {
 
@@ -27,6 +34,15 @@ function App() {
   const [isLoadingAddPlace, setIsLoadingAddPlace] = useState(false);
   const [isLoadingSetUserInfo, setIsLoadingSetUserInfo] = useState(false);
   const [isLoadingCardDelete, setIsLoadingCardDelete] = useState(false);
+
+  const [isLoggedIn, setLoggedIn] = useState(false)
+  const [userData, setUserData] = useState(null)
+
+  const [isSuccessfullSign, setIsSuccessfullSign] = useState(false);
+  const [isInfoTooltipOpen, setIsInfoTooltipOpen] = useState(false);
+
+
+  const navigate = useNavigate()
 
   useEffect(() => {
     api.getUserData(currentUser)
@@ -95,6 +111,10 @@ function App() {
     setAddPlaceOpen(true)
   }
 
+  function handleInfoTooltipOpen() {
+    setIsInfoTooltipOpen(true);
+  }
+
   function closeAllPopups() {
     setEditAvatarOpen(false)
     setEditProfileOpen(false)
@@ -102,6 +122,7 @@ function App() {
     setSelectedCard({})
     setImagePopupOpen(false)
     setPopupWithConfirmOpen(false)
+    setIsInfoTooltipOpen(false)
   }
 
   function handleUpdateUser(data) {
@@ -154,22 +175,100 @@ function App() {
     setPopupWithConfirmOpen(true)
   }
 
+
+  function handleLogin(email, password) {
+    auth.authorize(email, password)
+      .then((data) => {
+        localStorage.setItem("jwt", data.token)
+        setLoggedIn(true)
+        navigate('/mesto-react')
+      })
+  }
+
+  function handleRegister(email, password) {
+    auth.register(email, password)
+      .then((data) => {
+        setIsSuccessfullSign(true)
+        navigate("/sign-in")
+        handleInfoTooltipOpen()
+      })
+      .catch(err => {
+        console.log(err.message)
+        setIsSuccessfullSign(false)
+        handleInfoTooltipOpen()
+      })
+  }
+
+  function verifyToken() {
+    const token = localStorage.getItem("jwt")
+    auth.checkToken(token)
+      .then((data) => {
+        if (data) {
+          setUserData(data.data.email)
+          setLoggedIn(true)
+          navigate('/mesto-react')
+        }
+        else {
+          setLoggedIn(false)
+        }
+      })
+      .catch(err => {
+        console.log(err.message)
+      })
+  }
+
+  useEffect(() => {
+    verifyToken()
+    setUserData()
+  }, [])
+
+
+  function handleSignOut() {
+    setLoggedIn(false);
+    localStorage.removeItem('jwt');
+    navigate('/sign-in');
+  }
+
   return (
     <CurrentUserContext.Provider value={currentUser}>
       <CurrentCardsContext.Provider value={currentCards}>
         <div className="project">
           <div className="page">
-            <Header />
-            <Main
-              onEditProfile={handleEditProfileClick}
-              onAddPlace={handleAddPlaceClick}
-              onEditAvatar={handleEditAvatarClick}
-              onCardClick={handleCardClick}
-              user={currentUser}
-              cards={currentCards}
-              onCardLike={handleCardLike}
-              onCardDeleteRequest={handleCardDeleteConfirmation}
+
+            <Header
+              isLoggedIn={isLoggedIn}
+              userData={userData}
+
+              onSignOut={handleSignOut}
             />
+
+            <Routes>
+              <Route path="/mesto-react" element={<ProtectedRoute element={<Main
+                onEditProfile={handleEditProfileClick}
+                onAddPlace={handleAddPlaceClick}
+                onEditAvatar={handleEditAvatarClick}
+                onCardClick={handleCardClick}
+                user={currentUser}
+                cards={currentCards}
+                onCardLike={handleCardLike}
+                onCardDeleteRequest={handleCardDeleteConfirmation} />} isLoggedIn={isLoggedIn} />} />
+
+
+              <Route path="/sign-in" element={
+                <Login
+                  handleLogin={handleLogin}
+                />
+              } />
+
+              <Route path="/sign-up" element={
+                <Register
+                  handleRegister={handleRegister}
+
+                />
+              } />
+              <Route path="/" element={isLoggedIn ? <Navigate to='/mesto-react' replace /> : <Navigate to='/sign-in' replace />} />
+              <Route path='*' element={<h2>Ошибка 404</h2>} />
+            </Routes>
             <Footer />
           </div>
           <ImagePopup
@@ -204,7 +303,15 @@ function App() {
             onUpdateAvatar={handleUpdateAvatar}
             renderLoading={isLoadingAvatar} />
 
+            <InfoTooltip 
+            isOpen={isInfoTooltipOpen}
+            isSuccessfull = {isSuccessfullSign}
+            onClose= {closeAllPopups}
+
+            />
+
         </div>
+
       </CurrentCardsContext.Provider>
     </CurrentUserContext.Provider>
   );
--- src/Components/PopupWithForm/PopupWithForm.js
+++ src/Components/PopupWithForm/PopupWithForm.js
@@ -9,7 +9,7 @@ function PopupWithForm({ name, title, children, isOpen, onClose, buttonText, onS
         <form className="popup__container" name={`${name}`} onSubmit={onSubmit}>
           <fieldset className="popup__set">
             {children}
-            <button className="popup__savebutton" id="addcard" type="submit">{renderLoading ?
+            <button className="popup__savebutton" type="submit">{renderLoading ?
               loadingButtonText
               :
               buttonText}</button>
